c20a565f3d826eb462069d036d87a7b1efe61e83
LDMacKrell and Safiy Cham <safiy@>
2017-08-09 11:11:48 -0700

    update pre-existing code to define user interface code as distinct from back-end code.

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,9 @@ var divide = function(number1, number2) {
   return number1 / number2;
 }
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+$(document).ready(function() {
+	var number1 = parseInt(prompt("Enter a number:"));
+	var number2 = parseInt(prompt("Enter another number:"));
 
-var result = divide(number1, number2);
-alert(result);
+	alert(divide(number1, number2));
+});
